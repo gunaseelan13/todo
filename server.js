8ea4a57a160d7be93dbdde1e319829fb756d6d6e
Guna Seelan <guna.nehru.13@gmail.com>
2016-01-11 11:52:35 +0530

    Added delete using ID

--- server.js
+++ server.js
@@ -45,6 +45,18 @@ app.post('/todos', function (req, res){
 	res.json(body);
 });
 
+app.delete('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos,{id: todoId});
+
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todos found in id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 app.listen(PORT, function (){
 	console.log('Express listening on port '+PORT);
 });
