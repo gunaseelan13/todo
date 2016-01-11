22dbac6c5d380047aea10c11de1ce6a09e844d6d
Guna Seelan <guna.nehru.13@gmail.com>
2016-01-11 23:05:40 +0530

    Update function

--- server.js
+++ server.js
@@ -82,28 +82,29 @@ app.delete('/todos/:id', function (req, res){
 
 app.put('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 	var body =_.pick(req.body, 'description','completed');
-	var validAttribute = {};
+	var attribute = {};
 
-	 if(!matchedTodo){
-	 	return res.status(404).send();
+	 if(body.hasOwnProperty('completed')){
+	 	attribute.completed = body.completed;
 	 }
 
-	 if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-	 	validAttribute.completed = body.completed;
-	 } else if (body.hasOwnProperty('completed')) {
-	 	return res.status(400).send();
+	 if(body.hasOwnProperty('description')){
+	 	attribute.description =body.description;
 	 }
-
-	 if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
-	 	validAttribute.description =body.description;
-	 } else if (body.hasOwnProperty('description')) {
-	 	return res.status(400).send();
-	 }
-
-	 _.extend(matchedTodo, validAttribute);
-	 res.json(matchedTodo);
+	 db.todo.findById(todoId).then( function (todo){
+	 	if(todo){
+	 		return todo.update(attribute);
+	 	} else {
+	 		res.status(404).send();
+	 	}
+	 }, function (){
+	 	res.status(500).send();
+	 }).then( function (todo){
+			res.json(todo.toJSON());	 	
+	 }, function (e){
+	 	res.status(400).send();
+	 });
 });
 
 db.sequelize.sync().then( function (){
