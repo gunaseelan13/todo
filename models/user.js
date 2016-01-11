3c5815f8a86d7e18aeabd8525b5af027542c73b1
Guna Seelan <guna.nehru.13@gmail.com>
2016-01-12 00:29:14 +0530

    hook function added

--- models/user.js
+++ models/user.js
@@ -15,5 +15,13 @@ module.exports = function (sequelize, DataTypes){
 				len: [7, 200]
 			}
 		}
+	}, {
+		hooks: {
+			beforValidate: function (user, option){
+				if( typeof user.email === 'string'){
+					user.email = user.email.toLowerCase();
+				}
+			} 
+		}
 	});
 }
